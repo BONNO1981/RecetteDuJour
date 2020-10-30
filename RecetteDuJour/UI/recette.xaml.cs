5259b3dbdc18cb7be4786c055947d317d7ffad05
cnam <cnam@DESKTOP-7OV2INM>
2020-10-30 13:45:31 -1000

    deplacer dans le UI

--- RecetteDuJour/UI/recette.xaml.cs
+++ RecetteDuJour/UI/recette.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace RecetteDuJour
+namespace RecetteDuJour.UI
 {
     /// <summary>
     /// Interaction logic for recette.xaml
