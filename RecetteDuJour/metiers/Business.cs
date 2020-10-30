991719abf5fe221cee633fe0a4533d3d6ad74dc2
cnam <cnam@CNAM>
2020-10-30 13:10:55 -1000

    petite modification sur le main pour se reperer et rajout des parametre de la classe recette

--- RecetteDuJour/metiers/Business.cs
+++ RecetteDuJour/metiers/Business.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RecetteDuJour.metiers
 {
-    class Business
+    public class Business
     {
+        public static ObservableCollection<Recette> Recettes { get; set; }
+
+
     }
 }
--- RecetteDuJour/metiers/Recette.cs
+++ RecetteDuJour/metiers/Recette.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RecetteDuJour.metiers
 {
-    class Recette
+    public class Recette
     {
         int Id { get; set; }
         string Nom { get; set; }
@@ -19,8 +19,12 @@ namespace RecetteDuJour.metiers
 
         public Recette( string Nom, string Image, string Categorie, int NbPersonne,string Ingredients,string Preparation)
         {
-
-                
+            this.Nom = Nom;
+            this.Image = Image;
+            this.Categorie = Categorie;
+            this.NbPersonne = NbPersonne;
+            this.Ingredients = Ingredients;
+            this.Preparation = Preparation;
         }
 
     }
